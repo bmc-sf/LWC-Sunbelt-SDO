af68ed3871e3bf400fa48d0d24b78e8f81bc336e
Brian McCall <29308937+bmc-sf@users.noreply.github.com>
2021-06-29 17:55:34 -0400

    SDO commit

---
+++ force-app/main/default/lwc/sunbeltMobile/sunbeltMobile.js
@@ -0,0 +1,20 @@
+import { LightningElement } from 'lwc';
+//import CUSTOMER_INFORMATION from '@salesforce/resourceUrl/customer_information';
+//import RENTAL_METHOD from '@salesforce/resourceUrl/rental_method_and_duration';
+//import EQUIPMENT from '@salesforce/resourceUrl/equipment';
+//import FULFILLMENT_BRANCH from '@salesforce/resourceUrl/fullfillment_branch';
+//import RENTAL_INSTRUCTIONS from '@salesforce/resourceUrl/rental_instructions';
+
+export default class SunbeltMobile extends LightningElement {
+
+//    customerInfoImage = CUSTOMER_INFORMATION;
+//    rentalMethodImage = RENTAL_METHOD;
+//    equipmentImage = EQUIPMENT;
+//    fulfillmentBranchImage = FULFILLMENT_BRANCH;
+//    rentalInstructionsImage = RENTAL_INSTRUCTIONS;
+
+    handleClick() {
+        //add handler for clicking on action tile
+        //
+    }
+}
